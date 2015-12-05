b4edfe0f7c301139414064a5077975fe2ccc6c48
Ai Xin <scicomath@outlook.com>
2015-12-05 21:31:03 +0800

    画制动曲线示意图

--- problem1/brakingCurveScript.m
+++ problem1/brakingCurveScript.m
@@ -18,7 +18,7 @@ for i = 2:size(speedLimit,1)
     currentLimitSpeed = speedLimit(i,2);
     if currentLimitSpeed > endSpeed
         [ tempV,tempS,tempEreg ] = brakingCurveFun( s0, s1, endSpeed, gradient, curvature );
-        plot(tempS,tempV*3.6)
+        brakingLine = plot(tempS,tempV*3.6,'r');
         brakingCurveV = [brakingCurveV,tempV];
         brakingCurveS = [brakingCurveS,tempS];
         brakingCurveEreg = [brakingCurveEreg,tempEreg];
@@ -28,7 +28,7 @@ end
 %plot(S,V*3.6)
 %hold on
 for i = 1:size(speedLimit,1)
-    line(speedLimit(i,[1 3]),[speedLimit(i,2),speedLimit(i,2)])
+    limitLine = line(speedLimit(i,[1 3]),[speedLimit(i,2),speedLimit(i,2)]);
 end
 
 %% 选取车站A6到A7之间 画图
@@ -36,8 +36,13 @@ end
 ymin = 40;
 ymax = 90;
 %axis([stationP(7)-500,stationP(6)+500,ymin,ymax])
-axis([200,1500,0,ymax])
+axis([200,1500,ymin,ymax])
 set(gca,'XDir','reverse')
+xlabel('公里标（m）')
+ylabel('速度（km/h）')
+legend([limitLine,brakingLine],'速度限制','制动曲线')
+
+printFigureToPdf('制动曲线示意图.pdf', [11.5,8.5],'centimeters',[0 0  0 0]);
 % 添加车站标号
 %line([stationP(6),stationP(6)],[ymin,ymax],'LineStyle',':')
 %line([stationP(7),stationP(7)],[ymin,ymax],'LineStyle',':')
