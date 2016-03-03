64d6a54a77099e23d26c6e41330190ad715e7fa5
Ai Xin <scicomath@gmail.com>
2016-03-04 06:02:29 +0800

    整理第三问结果, 第一问重新画图

--- python/loadP3data.py
+++ python/loadP3data.py
@@ -4,7 +4,40 @@ Created on Sun Feb 28 23:17:14 2016
 
 @author: aixin
 """
+import numpy as np
 import pickle
 f = open("P3data.pckl","rb")
 delayTypeList,delayTimeList, delayNodeList, extraEList, lateNodeList,lateTimeList = pickle.load(f)
 f.close()
+
+num = len(delayTypeList)
+normDelayTime = 0.
+normLateTime = 0.
+normLateNum = 0
+normExtraE = 0.
+severeDelayTime = 0.
+severeLateTime = 0.
+severeLateNum = 0
+severeExtraE = 0.
+for i in range(num):
+    if delayTypeList[i] == 1:
+        normDelayTime += delayTimeList[i]
+        normExtraE += extraEList[i]
+        if lateTimeList[i]:
+            normLateTime += np.sum(lateTimeList[i])
+            normLateNum += len(lateNodeList[i])
+    elif delayTypeList[i] == 2:
+        severeDelayTime += delayTimeList[i]
+        severeExtraE += extraEList[i]
+        if lateTimeList[i]:
+            severeLateTime += np.sum(lateTimeList[i])
+            severeLateNum += len(lateNodeList[i])
+    else:
+        pass
+
+normExtraE = normExtraE / 3.6e6
+severeExtraE = severeExtraE / 3.6e6
+normNum = delayTypeList.count(1)
+severeNum = delayTypeList.count(2)
+print("普通延误: ", "平均延误时间 ", normDelayTime/normNum, "平均总晚点时间", normLateTime/normNum, "平均晚点站数", normLateNum/normNum, "平均额外消耗能量", normExtraE/normNum)
+print("严重延误: ", "平均延误时间 ", severeDelayTime/severeNum, "平均总晚点时间", severeLateTime/severeNum, "平均晚点站数", severeLateNum/severeNum, "平均额外消耗能量", severeExtraE/severeNum)
--- python/plotScript.py
+++ python/plotScript.py
@@ -17,9 +17,19 @@ f.close()
 
 
 def plotV(Sall):
+    params = {
+        'axes.labelsize': 12,
+        'font.size': 12,
+        'legend.fontsize': 12,
+        'xtick.labelsize': 12,
+        'ytick.labelsize': 12,
+        'text.usetex': False,
+        'figure.figsize': [4.5, 4.5],
+    }
+    rcParams.update(params)
     for i in range(Sall.secNum):
-        h1, = plot(Sall.S[i], Sall.V[i]*3.6, color="black") #, label=u'速度曲线'
-        h2, = plot(Sall.S[i], [Sall.secLimit[i]*3.6]*len(Sall.S[i]), color="black", linestyle="--")# , label=u'速度限制'
+        h1, = plot(Sall.S[i], Sall.V[i]*3.6, color="black", linewidth=1) #, label=u'速度曲线'
+        h2, = plot(Sall.S[i], [Sall.secLimit[i]*3.6]*len(Sall.S[i]), color="black", linewidth=1, linestyle="--")
     
     ylim(0.0,100.0)
     xlim(12100, 13600)
@@ -28,11 +38,15 @@ def plotV(Sall):
     ylabel('速度(m/s)')
     h1._label = "速度曲线"
     h2._label = "速度限制"
-    legend(loc='upper right') # 'upper right'
-    savefig("S6.png", dpi=600)
+    legend(loc='upper right')
+    savefig("S6.eps", dpi=600)
     show()
 
-def plotAllV(Sall, name):
+def plotAllV(Sall, name, figsize):
+    params = {
+        'figure.figsize': figsize,
+    }
+    rcParams.update(params)
     for i in range(Sall.num):
         for j in range(Sall.interSta[i].secNum):
             h1, = plot(Sall.interSta[i].S[j], Sall.interSta[i].V[j]*3.6, color="black")
@@ -44,10 +58,10 @@ def plotAllV(Sall, name):
     h1._label = "速度曲线"
     h2._label = "速度限制"
     legend()
-    savefig(name+".png", dpi=600)
+    savefig(name+".eps", dpi=600)
     show()
     
 plotV(S6)
-plotAllV(S67, "S67")
-plotAllV(Sall, "Sall")
+plotAllV(S67, "S67", [4.5, 4.5])
+plotAllV(Sall, "Sall", [6, 4.5])
 
